0e6c22985623b7b5b7c61a7f35e856567fb9d424
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-08-17 21:43:02 -0300

    Add Container with card

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 // eslint-disable-next-line no-unused-vars
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   return (
@@ -11,9 +12,19 @@ export default function Main() {
       <Header />
       <Content>
         <Card>
-          <CardhHeader></CardhHeader>
-          <CardContent></CardContent>
-          <CardFooter></CardFooter>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 2000.000,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R% 20,00 recebida de Jhony hoje às 06:00h
+            </Annotation>
+          </CardFooter>
         </Card>
       </Content>
 
