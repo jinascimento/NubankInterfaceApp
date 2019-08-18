0ff3117587d95dc7e227bad2f5244672a6fbf35b
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-08-17 21:22:38 -0300

    Add tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
 // eslint-disable-next-line no-unused-vars
 import { Container } from './styles';
 
@@ -8,6 +9,15 @@ export default function Main() {
   return (
     <Container>
       <Header />
+      <Content>
+        <Card>
+          <CardhHeader></CardhHeader>
+          <CardContent></CardContent>
+          <CardFooter></CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
