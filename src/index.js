03b2c55c84a470a5618c65aa1cc5bbdd4d181edb
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2020-02-04 20:12:14 -0300

    Add configs code push

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import CodePush from 'react-native-code-push';
 
 import '~/config/ReactotronConfig';
 
@@ -12,4 +13,6 @@ const App = () => (
   </>
 );
 
-export default App;
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(App);
